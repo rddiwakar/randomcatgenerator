4b6c77f431e9bcd260672d632a5f26bb90b8282e
rddiwakar <rahuldiwakar611@gmail.com>
2021-03-11 14:26:27 +0530

    fetching dog with cat api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("btn").addEventListener("click",(event) => {
+document.getElementById("btn1").addEventListener("click",(event) => {
     
     fetch('https://api.thecatapi.com/v1/images/search').then(res => res.json()).then(addingCat)
     event.preventDefault();
@@ -11,3 +11,15 @@ function addingCat(res){
  div.appendChild(img)
  img.setAttribute("src",(res[0].url))
 }
+document.getElementById("btn2").addEventListener("click",(event) => {
+    
+    fetch("https://dog.ceo/api/breeds/image/random").then(res => res.json()).then(addingDog)
+    event.preventDefault();
+})
+function addingDog(res){
+    let div = document.getElementById("container")
+    div.innerHTML = ""
+    let img = document.createElement("img")
+    div.appendChild(img)
+    img.setAttribute("src",(res.message))
+   }
